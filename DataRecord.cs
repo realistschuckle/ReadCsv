e33cb18ef3fc3a558175ab024e3236912d9183e9
Curtis Schlak <realistschuckle@gmail.com>
2016-11-11 16:24:06 -0500

    Sample project to read a CSV and output a pipe-delimited file.

---
+++ DataRecord.cs
@@ -0,0 +1,32 @@
+﻿namespace ReadCsv
+{
+    class DataRecord
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _rut;
+
+        public DataRecord(string firstName, string lastName, string email, string rut)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _rut = rut;
+        }
+
+        public string FirstName { get { return _firstName; } }
+
+        public string LastName { get { return _lastName; } }
+
+        public string Email { get { return _email; } }
+
+        public string Rut { get { return _rut; } }
+
+        public static DataRecord Parse(string line)
+        {
+            string[] entries = line.Split(',');
+            return new DataRecord(entries[0], entries[1], entries[2], entries[3]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DataRecord> records = new List<DataRecord>();
+            using (StreamReader input = File.OpenText("data.csv"))
+            {
+                for (string line = input.ReadLine(); line != null; line = input.ReadLine())
+                {
+                    records.Add(DataRecord.Parse(line));
+                }
+            }
+            records = records.OrderBy(x => x.LastName).ToList();
+            using (StreamWriter writer = File.CreateText("sorted.csv"))
+            {
+                writer.WriteLine("FirstName|LastName|Email|ChileanRutNumber");
+                foreach (var r in records)
+                {
+                    writer.WriteLine($"{r.FirstName}|{r.LastName}|{r.Email}|{r.Rut}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
